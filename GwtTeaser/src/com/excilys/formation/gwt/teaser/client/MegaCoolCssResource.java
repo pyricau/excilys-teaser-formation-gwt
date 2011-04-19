aef1b59b882703bba6bf2faa0c75569f83f90949
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-19 16:59:53 +0200

    Updated CSS related stuff

--- GwtTeaser/src/com/excilys/formation/gwt/teaser/client/MegaCoolCssResource.java
+++
@@ -1,8 +0,0 @@
-package com.excilys.formation.gwt.teaser.client;
-
-import com.google.gwt.resources.client.CssResource;
-
-interface MegaCoolCssResource extends CssResource {
-  String dockLayout();
-  String header();
-}
